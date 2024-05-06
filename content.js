a3e6422fc5db724fcebc01702d2177c1515de308
SvenningsenDev <jonasbuchart@gmail.com>
2024-05-06 11:46:41 +0200

    Working spoiler-free extension

--- content.js
+++ content.js
@@ -1,10 +1,12 @@
+
 chrome.storage.local.get('spoilersRemoved', function(data) {
     const selectors = [
         '.result-score', // result on /results page
         '.twoRowExtra', // small result in side bar
         '.won', //map score for winning team on match page
         '.lost', //map score for losing team on match page
-        '.mapholder' //small score under each map on match page
+        '.mapholder', //small score under each map on match page
+        '.matchstats' // scoreboard below stream
     ]
 
     selectors.forEach(selector => {
@@ -13,12 +15,37 @@ chrome.storage.local.get('spoilersRemoved', function(data) {
             el.style.visibility = 'visible';
         });
     });
-
-    removeSpoilersAddButton();
 });
 
-function removeSpoilersAddButton() {
-    const streamBoxes = document.querySelectorAll('.stream-box');
-    const numberOfStreamBoxes = streamBoxes.length;
-    console.log('Total number of stream boxes:', numberOfStreamBoxes);
+chrome.storage.local.get('spoilersRemoved', function(data) {
+    const spoilersRemoved = data.spoilersRemoved; // True or false
+    const streamBoxes = document.querySelectorAll('.spoiler > .stream-box');
+    
+    streamBoxes.forEach((box, index) => {
+        if (spoilersRemoved && index === 0) {
+            visible(box);            
+        } else if (!spoilersRemoved) {
+            visible(box);                      
+        } else {
+            box.style.visibility = 'hidden';            
+        }
+        const spoilerElement = box.querySelector('.spoiler');
+        if (!spoilerElement.dataset.originalText) {
+            spoilerElement.dataset.originalText = spoilerElement.textContent;
+        }
+    });
+
+    processHtml(streamBoxes[0].querySelector('.spoiler'), spoilersRemoved);
+});
+
+function processHtml(spoilerElement, spoilersRemoved) {
+    if (!spoilersRemoved) {
+        spoilerElement.textContent = spoilerElement.dataset.originalText; // Revert to original text
+    } else {
+        spoilerElement.textContent = "Start stream from map 1"; // Switch to alternate text
+    }
+}
+
+function visible(box) {
+    box.style.visibility = 'visible';
 }
--- popup.js
+++ popup.js
@@ -1,13 +1,30 @@
-document.getElementById('toggleSpoiler').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.storage.local.get('spoilersRemoved', function(data) {
-            const currentStatus = !data.spoilersRemoved;
-            console.log(currentStatus);  // Log the current status for debugging
-            chrome.storage.local.set({'spoilersRemoved': currentStatus}, function() {
-                // Inject the content script to read the new state and act on it
-                chrome.scripting.executeScript({
-                    target: {tabId: tabs[0].id},
-                    files: ['content.js']
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleButton = document.getElementById('toggleSpoiler');
+
+    // Function to update button text
+    function updateButtonText(areSpoilersRemoved) {
+        toggleButton.textContent = areSpoilersRemoved ? "Spoilers: Off" : "Spoilers: On";
+    }
+
+    // Fetch the initial state of spoilers and update button text
+    chrome.storage.local.get('spoilersRemoved', function(data) {
+        updateButtonText(data.spoilersRemoved);
+    });
+
+    toggleButton.addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.storage.local.get('spoilersRemoved', function(data) {
+                const currentStatus = !data.spoilersRemoved;
+
+                // Update the button text based on the new status
+                updateButtonText(currentStatus);
+
+                chrome.storage.local.set({'spoilersRemoved': currentStatus}, function() {
+                    // Inject the content script to read the new state and act on it
+                    chrome.scripting.executeScript({
+                        target: {tabId: tabs[0].id},
+                        files: ['content.js']
+                    });
                 });
             });
         });
